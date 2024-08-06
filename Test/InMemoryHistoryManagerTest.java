a8b127f390c59db5c11c9bcf47448fdb11d841e4
demamon <d-ba@yandex.ru>
2024-08-06 07:45:53 +0500

    Выполнение 6 ТЗ

--- Test/InMemoryHistoryManagerTest.java
+++ Test/InMemoryHistoryManagerTest.java
@@ -2,12 +2,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import manager.HistoryManager;
 import manager.ManagersUtils;
+import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 import tasks.TaskStatus;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InMemoryHistoryManagerTest {
     private static HistoryManager hm;
@@ -29,21 +30,40 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void addMaxSize() {
-        Task task = new Task("Test tasks.Task", "Test tasks.Task description", TaskStatus.NEW);
-
-        for (int i = 0; i < 10; i++) {
+    void getHistoryRepeatOneTask() {
+        Task task = new Task("Test tasks.Task", "Test tasks.Task description", TaskStatus.NEW, 1);
+        for (int i = 0; i < 6; i++) {
             hm.add(task);
         }
-
-        Task taskOverSize = new Task("Test Task1", "Test Task1 description", TaskStatus.IN_PROGRESS, 11);
-
-        hm.add(taskOverSize);
-
         final List<Task> history = hm.getHistory();
-
-        Task taskLastPosition = history.getLast();
-
-        assertEquals(taskLastPosition, taskOverSize, "Добавление 11 задачи некорректно.");
+        assertEquals(1, history.size(), "В истории просмотренных задач задачи не заменяются.");
     }
+
+    @Test
+    void getHistoryFirstAndLastAndMediumTasks() {
+        Task task = new Task("Test tasks.Task", "Test tasks.Task description", TaskStatus.NEW, 1);
+        Task task2 = new Task("Test tasks2", "Test tasks2 description", TaskStatus.NEW, 2);
+        Epic epic = new Epic("Test epic", "Test epic description", 3);
+        Subtask subtask = new Subtask("Test subtask", "Test subtask description", TaskStatus.NEW, 4, epic.getId());
+        hm.add(task2);
+        hm.add(epic);
+        hm.add(subtask);
+        hm.add(task2);
+        hm.add(task);
+        final List<Task> history = hm.getHistory();
+        assertEquals(task.getId(), history.getFirst().getId(), "Последний просмотренный элемент не верен.");
+        assertEquals(epic.getId(), history.getLast().getId(), "Первый просмотренный элемент не верен.");
+        assertEquals(subtask.getId(), history.get(2).getId(), "Средний просмотренный элемент не верен.");
+    }
+
+    @Test
+    void deleteTaskHistory() {
+        Task task = new Task("Test tasks.Task", "Test tasks.Task description", TaskStatus.NEW, 1);
+        hm.add(task);
+        hm.remove(1);
+        final List<Task> history = hm.getHistory();
+        assertNull(history, "Задача не удаляется");
+    }
+
+
 }
--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,27 +3,113 @@ package manager;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> historyViewedTasks = new ArrayList<>(10);
+    private static class HistoryLinkedList {
+        private HistoryListNode head;
+        private HistoryListNode tail;
+        private int size = 0;
+
+        private HistoryListNode linkLast(Task task) {
+            if (tail == null) {
+                tail = new HistoryListNode(task);
+                if (head == null) {
+                    head = tail;
+                }
+                size++;
+                return tail;
+            }
+            if (head == tail) {
+                tail = new HistoryListNode(task);
+                head.setNext(tail);
+                tail.setPrev(head);
+                size++;
+                return tail;
+            }
+            HistoryListNode node = new HistoryListNode(task);
+            tail.setNext(node);
+            node.setPrev(tail);
+            tail = node;
+            size++;
+            return tail;
+        }
+
+        private ArrayList<Task> getTasks() {
+            ArrayList<Task> tasks = new ArrayList<>();
+            tasks.add(tail.getData());
+            HistoryListNode node = tail.getPrev();
+            for (int i = 1; i < size; i++) {
+                tasks.add(node.getData());
+                if (!(node.getPrev() == null)) {
+                    node = node.getPrev();
+                }
+            }
+            return tasks;
+        }
+
+        private void removeNode(HistoryListNode node) {
+            HistoryListNode prevNode = node.getPrev();
+            HistoryListNode nextNode = node.getNext();
+            if (nextNode == null && (!(prevNode == null))) {
+                prevNode.setNext(null);
+                tail = prevNode;
+                size--;
+                return;
+            }
+            if (nextNode == null) {
+                tail = null;
+                size--;
+                return;
+            }
+            if (prevNode == null) {
+                node.setNext(null);
+                nextNode.setPrev(null);
+                head = nextNode;
+                size--;
+                return;
+            }
+            node.setPrev(null);
+            node.setNext(null);
+            prevNode.setNext(nextNode);
+            nextNode.setPrev(prevNode);
+            size--;
+        }
+    }
+
+    private final HistoryLinkedList historyLinkedList = new HistoryLinkedList();
+    private final Map<Integer, HistoryListNode> historyViewedTasks = new HashMap<>();
 
     InMemoryHistoryManager() {
     }
 
     @Override
     public void add(Task task) {
-        if (historyViewedTasks.size() == 10) {
-            historyViewedTasks.removeFirst();
+        if (historyViewedTasks.containsKey(task.getId())) {
+            HistoryListNode node = historyViewedTasks.get(task.getId());
+            historyLinkedList.removeNode(node);
         }
-            historyViewedTasks.add(task);
+        historyViewedTasks.put(task.getId(), historyLinkedList.linkLast(task));
     }
 
     @Override
     public List<Task> getHistory() {
         if (historyViewedTasks.isEmpty()) {
             System.out.println("Список просмотренных задач пуст");
+            return null;
         }
-        return new ArrayList<>(historyViewedTasks);
+        return new ArrayList<>(historyLinkedList.getTasks());
+    }
+
+    @Override
+    public void remove(int id) {
+        if (!(historyViewedTasks.get(id) == null)) {
+            HistoryListNode node = historyViewedTasks.get(id);
+            historyLinkedList.removeNode(node);
+            historyViewedTasks.remove(id);
+
+        }
     }
 }
