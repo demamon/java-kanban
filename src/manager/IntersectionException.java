c3e6c8494373540a28de3443c17a9d94573b3d09
demamon <d-ba@yandex.ru>
2024-09-21 12:56:47 +0500

    ВЫполнил 9 ТЗ

--- src/manager/IntersectionException.java
+++ src/manager/IntersectionException.java
@@ -1,6 +1,6 @@
 package manager;
 
-public class IntersectionException extends RuntimeException{
+public class IntersectionException extends RuntimeException {
     public IntersectionException(final String message) {
         super(message);
     }
